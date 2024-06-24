220501a6123c18ab838f92dd0ba558ab3ae5f909
Thiago Elias <thiagoelias99@gmail.com>
2024-06-24 11:16:11 -0300

    feat: :sparkles: RFID
    When RFID tags is reded, display its id on the LCD for a brief moment. Serial send tag Id

--- readme.md
+++ readme.md
@@ -3,16 +3,20 @@
 - [X] Serial send humidity && temperature.
 - [X] Monitor joystick X | Y position, display it on the LCD.
 - [X] Serial send joystick X | Y position.
-- [ ] When RFID tags is reded, display its id on the LCD for a brief moment.
-- [ ] Serial send tag Id.
+- [X] When RFID tags is reded, display its id on the LCD for a brief moment.
+- [X] Serial send tag Id.
+- [ ] Bip when serial data is received.
+- [ ] Bip when RFID tag is reded.
 
 ### Serial Patterns
-- Humidity (send): 44 48 48 55 (VALUE IN HEX)
+- ***Humidity*** (send): 44 48 48 55 (VALUE IN HEX)
   - 44 48 48 55 3C -> 60%
   - 44 48 48 55 14 -> 20%
-- Temperature (send): 44 48 54 45 (VALUE IN HEX)
+- ***Temperature*** (send): 44 48 54 45 (VALUE IN HEX)
   - 44 48 54 45 19 -> 25%
   - 44 48 54 45 25 -> 37%
-- Joystick (send): 4A 59 58 49 (X VALUE IN HEX) (Y VALUE IN HEX)
+- ***Joystick*** (send): 4A 59 58 49 (X VALUE IN HEX) (Y VALUE IN HEX)
   - 4A 59 58 49 1E 36 -> X=30 Y=70
   - 4A 59 58 49 32 00 -> X=50 Y=0
+- ***RFID*** (send): 52 46 49 44 (ID IN HEX)
+  - 52 46 49 44 53 58 68 20 -> Tag ID = 83 88 104 32
--- serial/serial.ino
+++ serial/serial.ino
@@ -1,5 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 #include <DHT11.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 // LCD
 #define LCD_ADDRESS 0x27
@@ -21,6 +23,18 @@ int dht_read_count = 0;
 #define JOY_Y A2
 int joy_x = 0;
 int joy_y = 0;
+
+// RFID
+#define RF_SS_PIN 10
+#define RF_RST_PIN 9
+MFRC522 mfrc522(RF_SS_PIN, RF_RST_PIN);
+byte rf_read_iu[4];
+
+void rfid_setup()
+{
+  SPI.begin();
+  mfrc522.PCD_Init();
+}
 
 String command = "";
 
@@ -99,11 +113,49 @@ void joystick_read()
   joy_y = map(analogRead(JOY_Y), 0, 1023, 0, 100);
 }
 
+void rfid_read()
+{
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  for (byte i = 0; i < 4; i++)
+  {
+    rf_read_iu[i] = mfrc522.uid.uidByte[i];
+  }
+  mfrc522.PICC_HaltA();
+
+  Serial.print("52 46 49 44 ");
+  for (byte i = 0; i < 4; i++)
+  {
+    Serial.print(rf_read_iu[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  lcd.clear();
+  lcd.print("RFID: ");
+  lcd.setCursor(0, 1);
+  for (byte i = 0; i < 4; i++)
+  {
+    lcd.print(rf_read_iu[i]);
+    lcd.print(" ");
+  }
+
+  delay(1000);
+  lcd.clear();
+}
+
 void setup()
 {
   lcd_setup();
   serial_setup();
   joystick_setup();
+  rfid_setup();
 }
 
 void loop()
@@ -119,6 +171,7 @@ void loop()
   joystick_read();
   lcd_print_general_info();
   serial_send_general_info();
+  rfid_read();
 
   delay(100);
 }
