33d6e62c6b2fc629f1ebac16381bc4c6ea1aecb5
Thiago Elias <thiagoelias99@gmail.com>
2024-06-24 10:48:50 -0300

    feat: :sparkles: Implement Joystick
    Monitor joystick X | Y position, display it on the LCD. Serial send joystick X | Y position.

--- serial/serial.ino
+++ serial/serial.ino
@@ -13,6 +13,14 @@ LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_LINES);
 DHT11 dht11(3);
 int temperature = 0;
 int humidity = 0;
+int dht_read_interval = 100;
+int dht_read_count = 0;
+
+// JoyStick
+#define JOY_X A1
+#define JOY_Y A2
+int joy_x = 0;
+int joy_y = 0;
 
 String command = "";
 
@@ -33,7 +41,6 @@ void lcd_print_serial_command()
 
 void lcd_print_general_info()
 {
-  // lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("T: ");
   lcd.print(temperature);
@@ -42,6 +49,12 @@ void lcd_print_general_info()
   lcd.print("H: ");
   lcd.print(humidity);
   lcd.print("%");
+  lcd.setCursor(11, 0);
+  lcd.print("X: ");
+  lcd.print(joy_x);
+  lcd.setCursor(11, 1);
+  lcd.print("Y: ");
+  lcd.print(joy_y);
 }
 
 void serial_setup()
@@ -55,17 +68,42 @@ void serial_send_general_info()
   Serial.println(humidity, HEX);
   Serial.print("44 48 54 45 ");
   Serial.println(temperature, HEX);
+  Serial.print("4A 59 58 49 ");
+  Serial.print(joy_x, HEX);
+  Serial.print(" ");
+  Serial.println(joy_y, HEX);
 }
 
 void dht11_read()
 {
-  dht11.readTemperatureHumidity(temperature, humidity);
+  if (dht_read_count == 0)
+  {
+    dht11.readTemperatureHumidity(temperature, humidity);
+    dht_read_count = dht_read_interval;
+  }
+  else
+  {
+    dht_read_count--;
+  }
+}
+
+void joystick_setup()
+{
+  pinMode(JOY_X, INPUT);
+  pinMode(JOY_Y, INPUT);
+}
+
+void joystick_read()
+{
+  joy_x = map(analogRead(JOY_X), 0, 1023, 0, 100);
+  joy_y = map(analogRead(JOY_Y), 0, 1023, 0, 100);
 }
 
 void setup()
 {
   lcd_setup();
   serial_setup();
+  joystick_setup();
 }
 
 void loop()
@@ -78,6 +116,9 @@ void loop()
     lcd_print_serial_command();
   }
   dht11_read();
+  joystick_read();
   lcd_print_general_info();
   serial_send_general_info();
+
+  delay(100);
 }
